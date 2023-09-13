d1475fc9b20599d1dc3d06f89620fc4e2c92a67a
maintains00 <wltjs2587@gmail.com>
2023-09-13 22:11:32 +0900

    crud

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -120,7 +122,16 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,84 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
+from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
-
 from posts.models import Post
 
 def index(request):
-    return render(request, 'index.html')
+    post_list = Post.objects.all().order_by('-created_at') # Post 전체 데이터 조회
+    context = {
+        'post_list' : post_list
+    }
+    return render(request, 'index.html', context)
 
 def post_list_view(request):
-    return render(request, 'posts/post_list.html')
+    post_list = Post.objects.all() # Post 전체 데이터 조회
+    # post_list = Post.objects.filter(writer=request.user) # Post.writer가 현재 로그인인 것 조회
+    context = {
+        'post_list' : post_list
+    }
+    return render(request, 'posts/post_list.html', context)
 
+@login_required
 def post_create_view(request):
-    return render(request, 'posts/post_form.html')
+    if request.method == 'GET':
+        return render(request, 'posts/post_form.html')
+    else:
+        image = request.FILES.get('image')
+        content = request.POST.get('content')
+        # print(image)
+        # print(content)
+        Post.objects.create(
+            image=image,
+            content=content,
+            writer=request.user
+        )
+        return redirect('index')
 
 def post_detail_view(request, id):
-    return render(request, 'posts/post_detail.html')
+    try: 
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        return redirect('index')
+    
+    context = {
+        'post' : post,
+    }
+    return render(request, 'posts/post_detail.html', context)
 
+@login_required
 def post_update_view(request, id):
-    return render(request, 'posts/post_form.html')
+    # post = Post.objects.get(id=id)
+    post = get_object_or_404(Post, id=id, writer=request.user)
+    
+    if request.method == 'GET':
+        context={'post':post}
+        return render(request, 'posts/post_form.html', context)
+    elif request.method == 'POST':
+        new_image = request.FILES.get('image')
+        content = request.POST.get('content')
+        # print(new_image)
+        # print(content)
+        if new_image:
+            post.image.delete()
+            post.image = new_image
+        post.content = content
+        post.save()
+        return redirect('posts:post-detail', post.id)
 
+@login_required
 def post_delete_view(request, id):
-    return render(request, 'posts/post_confirm_delete.html')
+    post = get_object_or_404(Post, id=id, writer=request.user)
+    if request.user != post.writer:
+        return Http404('잘못된 접근입니다.')
+    
+    if request.method == 'GET':
+        context={'post':post}
+        return render(request, 'posts/post_confirm_delete.html',context)
+    else:
+        post.delete()
+        return redirect('index')
+
 
 
 # fbv
